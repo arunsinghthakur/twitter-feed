e3cdd89c7152210ed5f8c767ffce99d5dcebd911
arunsingh <arun_gkv@yahoo.co.in>
2018-03-21 16:31:20 +0530

    initial commit

---
+++ src/main/java/com/joker/twiterfeed/twiterfeed/TwiterFeedApplication.java
@@ -0,0 +1,12 @@
+package com.joker.twiterfeed.twiterfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiterFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiterFeedApplication.class, args);
+	}
+}
