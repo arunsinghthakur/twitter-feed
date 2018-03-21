f70aba8a061ce1a760a85349df797cb1ed60ac17
arunsingh <arun_gkv@yahoo.co.in>
2018-03-21 16:34:04 +0530

    package change

--- src/main/java/com/joker/twitterfeed/TwiterFeedApplication.java
+++ src/main/java/com/joker/twitterfeed/TwiterFeedApplication.java
@@ -1,4 +1,4 @@
-package com.joker.twiterfeed.twiterfeed;
+package com.joker.twitterfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
